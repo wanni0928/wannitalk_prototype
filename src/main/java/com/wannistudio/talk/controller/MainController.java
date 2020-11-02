f2b3fa5425be99359e01ab9e204678c1c531fee0
wanni0928 <chlgudwnd123@gmail.com>
2020-11-02 13:09:11 +0900

    Update MainController.java

--- src/main/java/com/wannistudio/talk/controller/MainController.java
+++ src/main/java/com/wannistudio/talk/controller/MainController.java
@@ -1,16 +1,82 @@
 package com.wannistudio.talk.controller;
 
+import com.wannistudio.talk.domain.Room;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class MainController {
+
+    List<Room> rooms = new ArrayList<>();
+    static int roomNumber = 0;
 
     @RequestMapping("/chat")
     public ModelAndView ModelAndView() {
         ModelAndView mv = new ModelAndView();
         mv.setViewName("chat");
+        return mv;
+    }
+
+    /**
+     * 방 페이지
+     */
+    @RequestMapping("/room")
+    public ModelAndView room() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("room");
+        return mv;
+    }
+
+    /**
+     * 방 생성하기
+     */
+    @RequestMapping("/createRoom")
+    public @ResponseBody List<Room> createRoom(@RequestParam HashMap<Object, Object> params){
+        String roomName = (String) params.get("roomName");
+        if(roomName != null && !roomName.trim().equals("")) {
+            Room room = new Room();
+            room.setRoomNumber(++roomNumber);
+            room.setRoomName(roomName);
+            rooms.add(room);
+        }
+        return rooms;
+    }
+
+    /**
+     * 방 정보가져오기
+     * @param params
+     * @return
+     */
+    @RequestMapping("/getRoom")
+    public @ResponseBody List<Room> getRoom(@RequestParam HashMap<Object, Object> params){
+        return rooms;
+    }
+
+    /**
+     * 채팅방
+     * @return
+     */
+    @RequestMapping("/moveChatting")
+    public ModelAndView chatting(@RequestParam HashMap<Object, Object> params) {
+        ModelAndView mv = new ModelAndView();
+        int roomNumber = Integer.parseInt((String) params.get("roomNumber"));
+
+        List<Room> new_list = rooms.stream().filter(o->o.getRoomNumber()==roomNumber).collect(Collectors.toList());
+        if(new_list != null && new_list.size() > 0) {
+            mv.addObject("roomName", params.get("roomName"));
+            mv.addObject("roomNumber", params.get("roomNumber"));
+            mv.setViewName("chat");
+        }else {
+            mv.setViewName("room");
+        }
         return mv;
     }
 }
