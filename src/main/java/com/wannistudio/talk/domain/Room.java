018fe247febe2670e2baa0365dd369d1bc4d1dcf
wanni0928 <chlgudwnd123@gmail.com>
2020-11-02 13:09:14 +0900

    Create Room.java

---
+++ src/main/java/com/wannistudio/talk/domain/Room.java
@@ -0,0 +1,10 @@
+package com.wannistudio.talk.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Room {
+    private int roomNumber;
+    private String roomName;
+}
