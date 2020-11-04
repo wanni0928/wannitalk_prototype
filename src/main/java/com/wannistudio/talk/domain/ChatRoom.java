79c2ef3198010f45c29cd6ef1287b7ef7f6e9482
wanni0928 <chlgudwnd123@gmail.com>
2020-11-04 15:16:30 +0900

    Update ChatRoom.java

--- src/main/java/com/wannistudio/talk/domain/ChatRoom.java
+++ src/main/java/com/wannistudio/talk/domain/ChatRoom.java
@@ -1,50 +1,21 @@
 package com.wannistudio.talk.domain;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketSession;
 
-import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.UUID;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
+@Entity
 @Getter @Setter
+@NoArgsConstructor
 public class ChatRoom {
-    private String roomId;
-    private String name;
-    private Set<WebSocketSession> sessions = new HashSet<>();
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "room_id")
+    private Long id;
 
-    public static ChatRoom create(String name){
-        ChatRoom chatRoom = new ChatRoom();
-        chatRoom.roomId = UUID.randomUUID().toString();
-        chatRoom.name = name;
-        return chatRoom;
-    }
-
-    public void handleMessage(WebSocketSession session, ChatMessage chatMessage, ObjectMapper objectMapper) throws IOException {
-        switch (chatMessage.getType()) {
-            case ENTER:
-                sessions.add(session);
-                chatMessage.setMessage(chatMessage.getWriter() + "님이 입장하셨습니다.");
-                break;
-            case LEAVE:
-                sessions.remove(session);
-                chatMessage.setMessage(chatMessage.getWriter() + "님임 퇴장하셨습니다.");
-                break;
-            default:
-                chatMessage.setMessage(chatMessage.getWriter() + " : " + chatMessage.getMessage());
-                break;
-        }
-        send(chatMessage, objectMapper);
-    }
-
-    private void send(ChatMessage chatMessage, ObjectMapper objectMapper) throws IOException {
-        TextMessage textMessage = new TextMessage(objectMapper.writeValueAsString(chatMessage.getMessage()));
-        for(WebSocketSession session : sessions){
-            session.sendMessage(textMessage);
-        }
-    }
+    @OneToMany(mappedBy = "chatRoom", cascade = CascadeType.ALL)
+    private List<ChatMessage> messages = new ArrayList<>();
 }
