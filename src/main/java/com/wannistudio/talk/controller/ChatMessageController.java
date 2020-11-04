24e7f6d3360ace676343427ef1b575c56d85df69
wanni0928 <chlgudwnd123@gmail.com>
2020-11-04 15:16:09 +0900

    Create ChatMessageController.java

---
+++ src/main/java/com/wannistudio/talk/controller/ChatMessageController.java
@@ -0,0 +1,45 @@
+package com.wannistudio.talk.controller;
+
+import com.wannistudio.talk.domain.Member;
+import com.wannistudio.talk.service.ChatMessageService;
+import com.wannistudio.talk.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ChatMessageController {
+    @Autowired
+    private final SimpMessagingTemplate simpMessagingTemplate;
+    private final ChatMessageService chatMessageService;
+    private final MemberService memberService;
+
+//    @MessageMapping("/chat/send")
+//    public void sendMsg(ChatMessageForm form) {
+//        ChatMessage chatMessage = new ChatMessage();
+//        chatMessage.setMember(memberService.findOne(11L));
+//        chatMessage.setMessage(form.getMessage());
+//        chatMessage.setTime(LocalDateTime.now());
+//        chatMessageService.sendMsg(chatMessage);
+//        simpMessagingTemplate.convertAndSend("/topic/" + form.getName(), form);
+//    }
+//
+    @GetMapping("/chat")
+    public String chatHome(Model model) {
+        Member member = memberService.findOne(11L);
+        model.addAttribute("member", member);
+        return "chat/main";
+    }
+
+    @PostMapping("/send")
+    public String sendData(String name, Model model){
+        Object name1 = model.getAttribute("name");
+        System.out.println(name1.toString());
+        return "success";
+    }
+}
